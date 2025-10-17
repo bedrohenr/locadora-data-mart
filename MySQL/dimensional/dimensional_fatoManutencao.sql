d03ff16be333b627ea4913b1e5e99076130c7e19
Pedro Ferreira <ferfonhp@gmail.com>
2025-10-17 16:13:22 -0300

    feat: export database dimensional

---
+++ MySQL/dimensional/dimensional_fatoManutencao.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: dimensional
+-- ------------------------------------------------------
+-- Server version	8.0.43
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `fatoManutencao`
+--
+
+DROP TABLE IF EXISTS `fatoManutencao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `fatoManutencao` (
+  `dimVeiculo_codVeiculo` int DEFAULT NULL,
+  `dimServico_codServico` int DEFAULT NULL,
+  `dimData_dataSaida` int DEFAULT NULL,
+  `dimData_dataConclusao` int DEFAULT NULL,
+  `dimData_dataRetorno` int DEFAULT NULL,
+  `vlrServico` double DEFAULT NULL,
+  `vlrTotalManutencao` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fatoManutencao`
+--
+
+LOCK TABLES `fatoManutencao` WRITE;
+/*!40000 ALTER TABLE `fatoManutencao` DISABLE KEYS */;
+INSERT INTO `fatoManutencao` VALUES (1,1,20171220,20171228,20171230,150,230),(1,2,20171220,20171229,20171230,80,230),(2,3,20180105,20180106,20180107,120,120),(4,4,20180110,20180119,20180120,350,350),(5,5,20180305,20180308,20180310,95,95);
+/*!40000 ALTER TABLE `fatoManutencao` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-10-17 16:13:02
